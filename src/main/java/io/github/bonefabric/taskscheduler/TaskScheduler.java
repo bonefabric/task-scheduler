dc9c30d21b324e9f71705e4e1e199cc9d9f618f0
bonefabric <iburanguloff@yandex.ru>
2024-12-24 20:45:33 +0500

    application main class and config

---
+++ src/main/java/io/github/bonefabric/taskscheduler/TaskScheduler.java
@@ -0,0 +1,12 @@
+package io.github.bonefabric.taskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskScheduler {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskScheduler.class, args);
+    }
+}
---
+++ src/main/java/io/github/bonefabric/taskscheduler/config/Config.java
@@ -0,0 +1,7 @@
+package io.github.bonefabric.taskscheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+}
